962e8a7f66c4e48aa07ac6993ce0a7053896c96d
Douchebag <ingvar@ingvar.tw>
2024-10-18 13:55:17 +0000

    eslint errors fixed

--- scripts.js
+++ scripts.js
@@ -1,18 +1,75 @@
+import { longest } from './lib/longest.js';
+import { shortest } from './lib/shortest.js';
+import { reverse } from './lib/reverse.js';
+import { palindrome } from './lib/palindrome.js';
+import { vowels } from './lib/vowels.js';
+import { consonants } from './lib/consonants.js';
+
+const textAreaElement = document.querySelector('textarea');
+const formElement = document.querySelector('form');
+const outputElement = document.querySelector('.hidden');
+const wideElement = document.querySelectorAll('.wide');
+const longestElement = document.querySelector('.longest');
+const shortestElement = document.querySelector('.shortest');
+const vowelsElement = document.querySelector('.vowels');
+const consonantsElement = document.querySelector('.consonants');
+const palindromeElement = document.querySelector('.palindrome');
+
+let looping = false;
+
 /**
- * Sýnilausn á verkefni 8 í Vefforritun 1, 2024.
- * Byggir á sýnilausn á verkefni 7.
- * Notar jsdoc til að skrifa lýsingu á föllum, inntaki og úttaki.
- * Kveikið á `Check JS` í Visual Studio Code til að sjá mögulegar villur.
- * Notar `console.assert` til að athuga hvort föll virki rétt.
+ * resetar öllum gildum, felur output aftur og setur looping í false  
  */
+function resetHandler() {
+    outputElement.classList.add('hidden');
+    textAreaElement.value = '';
+    wideElement[0].textContent = '';
+    longestElement.textContent = '';
+    shortestElement.textContent = '';
+    vowelsElement.textContent = '';
+    consonantsElement.textContent = '';
+    palindromeElement.textContent = '';
+    wideElement[1].textContent = '';
 
-import { isString, splitOnWhitespace } from './lib/helpers.js';
+    looping = false; 
+}
 
-const test = isString('hæ');
-console.log('test er strengur?', test);
+/**
+ * uppfærir gildi í outputi
+ */
+function updateValues() {
+  if (looping) { 
+    const text = textAreaElement.value;
+    const longestWord = longest(text);
+    const shortestWord = shortest(text);
+    const reversedText = reverse(text);
+    const isPalindrome = palindrome(text);
+    const vowelCount = vowels(text);
+    const consonantCount = consonants(text);
+    
+    wideElement[0].textContent = text;
+    longestElement.textContent = longestWord;
+    shortestElement.textContent = shortestWord;
+    vowelsElement.textContent = vowelCount;
+    consonantsElement.textContent = consonantCount;
+    palindromeElement.textContent = isPalindrome ? '' : 'ekki';
+    wideElement[1].textContent = reversedText;
+  }
+}
 
-const stringWithWhitespace = `halló
-\theimur
-hæ`;
-const split = splitOnWhitespace(stringWithWhitespace);
-console.log(split);
+/**
+ * birtir output, setur looping í true og kallar a updateValues
+ * @param {greining takki} event 
+ */
+function submitHandler(event) {
+    event.preventDefault();
+    outputElement.classList.remove('hidden');
+    looping = true; 
+    updateValues();
+}
+
+
+textAreaElement.addEventListener('input', updateValues);
+
+formElement.addEventListener('submit', submitHandler);
+formElement.addEventListener('reset', resetHandler);
